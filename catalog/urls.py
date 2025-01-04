06fae7d87248a6e3712ef3f28e8099e667f77b30
ku93 <tkachev1993adg@yandex.ru>
2025-01-04 05:55:51 +0300

    django_start_2

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,10 @@ from .views import home, contacts
 app_name = CatalogConfig.name
 urlpatterns = [
     path("", home, name='home'),
-    path("contacts/", contacts, name='contact')
+    path("contacts/", contacts, name='contact'),
+    path("categories/", contacts, name='categories'),
+    path("categories/category_1/", contacts, name='category_1'),
+    path("categories/category_2/", contacts, name='category_2'),
+    path("categories/category_3/", contacts, name='category_3'),
+    path("categories/category_4/", contacts, name='category_4')
 ]
