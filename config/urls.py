3293ed3aaaa396b1c43d158d4c32798cd9b4760a
ku93 <tkachev1993adg@yandex.ru>
2025-02-01 04:30:48 +0300

    django_wb_8

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("catalog.urls", namespace='catalog')),
-    path("", include("blog.urls", namespace='blog')),
+    path("blog/", include("blog.urls", namespace='blog')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
