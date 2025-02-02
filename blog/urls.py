138729f5b4b2d2bff40c1ad6140cb0d3da49ade1
ku93 <tkachev1993adg@yandex.ru>
2025-02-02 14:22:57 +0300

    django_wb_9

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, Home, search_results
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('blog/', Home.as_view(), name='home'),
     path('blog/list/', PostListView.as_view(), name='blog'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('search/', search_results, name='search_results'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,22 +4,30 @@ from django.urls import reverse_lazy
 
 from .models import BlogPost
 
-class Home(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'blog/home.html')
 
 class PostListView(ListView):
     model = BlogPost
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+
     def get_queryset(self):
         return BlogPost.objects.filter(is_published=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = 'blog'
+        return context
 
 class PostDetailView(DetailView):
     model = BlogPost
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = 'blog'
+        return context
 
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
@@ -27,11 +35,21 @@ class PostDetailView(DetailView):
         obj.save()
         return obj
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = 'blog'
+        return context
+
 class PostCreateView(CreateView):
     model = BlogPost
     fields = ['title', 'content', 'preview_image', 'is_published']
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('blog:blog')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = 'blog'
+        return context
 
 class PostUpdateView(UpdateView):
     model = BlogPost
@@ -39,15 +57,17 @@ class PostUpdateView(UpdateView):
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('blog:blog')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = 'blog'
+        return context
+
 class PostDeleteView(DeleteView):
     model = BlogPost
     success_url = reverse_lazy('blog:blog')
     template_name = 'blog/post_confirm_delete.html'
 
-def search_results(request):
-    query = request.GET.get('q')
-    results = []
-    if query:
-        posts = BlogPost.objects.filter(title__icontains=query)
-        results = posts
-    return render(request, 'blog/search_results.html', {'results': results})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_page'] = 'blog'
+        return context
