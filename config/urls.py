3c9bd5b93c1beb6f6b9d01f0a27c8099ecf0b2ac
ku93 <tkachev1993adg@yandex.ru>
2025-02-12 18:39:52 +0300

    auth

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("catalog.urls", namespace='catalog')),
     path("blog/", include("blog.urls", namespace='blog')),
+    path("users/", include("users.urls", namespace='users')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
---
+++ users/models.py
@@ -0,0 +1,25 @@
+import secrets
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Адрес электронной почты")
+
+    phone_number = PhoneNumberField(blank=True, null=True, verbose_name="Номер телефона", help_text="Введите номер телефона")
+    avatar = models.ImageField(upload_to="users/avatars/", blank=True, null=True, verbose_name="Аватар")
+    country = models.CharField(max_length=100, blank=True, null=True, verbose_name="Страна")
+
+    token = models.CharField(max_length=100, verbose_name= "Token", blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
